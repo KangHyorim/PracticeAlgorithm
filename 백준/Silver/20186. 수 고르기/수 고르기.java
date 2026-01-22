747c72e9b4a372075f56cda54ffbec3983c03995
KangHyorim <80194553+KangHyorim@users.noreply.github.com>
2026-01-22 22:32:42 +0900

    [Silver IV] Title: 수 고르기, Time: 136 ms, Memory: 15504 KB, Score: 31 point -BaekjoonHub

---
+++ 백준/Silver/20186. 수 고르기/수 고르기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int score = 0;
+        for (int i = 0; i < K && i < N; i++) {
+            int val = arr[N - i - 1] - i;
+            if (val <= 0) break;
+            score += val;
+        }
+        System.out.println(score);
+    }
+}
