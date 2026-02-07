436aec30f2ba2a61a6f0e4705e9e07aa3c8d2e1f
KangHyorim <80194553+KangHyorim@users.noreply.github.com>
2026-02-07 21:20:14 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -3,20 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        List<Integer> server = new ArrayList<>();
+        int[] del = new int[24];
+        
+        int current = 0;
         
         for(int i = 0; i < players.length; i++){
-            int time = i;
-            server.removeIf(endTime -> endTime == time);
+            current -= del[i];
             
-            int p = players[i];
-            int capacity = (server.size() + 1) * m - 1;
+            int capacity = (current + 1) * m - 1;
             
-            while(p > capacity) {
-                server.add(i + k);
-                answer++;
+            if(players[i] > capacity) {
+                int needed = players[i] - capacity;
                 
-                capacity += m;
+                int add = (needed + m - 1) / m;
+                
+                answer += add;
+                current += add;
+                
+                if(i+k < 24){
+                    del[i+k] += add;
+                }
             }
         }
     
