2f06b9b6969385814e9c5964708cc08a4b0daba5
KangHyorim <80194553+KangHyorim@users.noreply.github.com>
2026-04-06 21:07:48 +0900

    [Silver II] Title: 창고 다각형, Time: 124 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        int[] height = new int[1001];
+        int minL = 1001, maxL = 0;
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());   // 왼쪽 면의 위치
+            int H = Integer.parseInt(st.nextToken());   // 높이
+
+            height[L] = H;
+            minL = Math.min(minL, L);
+            maxL = Math.max(maxL, L);
+        }
+
+        int[] left = new int[1001];
+        int currentLeftMax = 0;
+        for (int i = minL; i <= maxL; i++) {
+            currentLeftMax = Math.max(currentLeftMax, height[i]);
+            left[i] = currentLeftMax;
+        }
+
+        int[] right = new int[1001];
+        int currentRightMax = 0;
+        for (int i = maxL; i >= minL; i--) {
+            currentRightMax = Math.max(currentRightMax, height[i]);
+            right[i] = currentRightMax;
+        }
+
+        int ans = 0;
+        for (int i = minL; i <= maxL; i++) {
+            ans += Math.min(left[i], right[i]);
+        }
+
+        System.out.println(ans);
+    }
+}
