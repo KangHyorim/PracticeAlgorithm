62a320ef882ca776ee912368288108726941d749
KangHyorim <80194553+KangHyorim@users.noreply.github.com>
2026-04-28 18:28:49 +0900

    [level 2] Title: 올바른 괄호, Time: 6.48 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean solution(String s) {
-        int count = 0;
+        int cnt = 0;
         
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                count++;
-            } else {  // ')'
-                count--;
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                cnt++;
+            }else {
+                cnt--;
             }
             
-            // ')'가 더 많아진 순간 → 실패
-            if (count < 0) return false;
+            if(cnt < 0) return false;
         }
         
-        // 모든 괄호가 짝이 맞아 떨어졌는가?
-        return count == 0;
+        return cnt == 0;
     }
 }
