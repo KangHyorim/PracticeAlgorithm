035f2809fb38e2bec690b48e2d6da73745bc7ca3
KangHyorim <80194553+KangHyorim@users.noreply.github.com>
2026-04-28 19:08:08 +0900

    [level 2] Title: 더 맵게, Time: 1675.76 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq =  new PriorityQueue<>();
+        
+        for(int sc : scoville){
+            pq.add(sc);
+        }
+        
+        int answer = 0;
+
+        while(pq.size() >= 2 && pq.peek() < K){
+            answer++;
+            int first = pq.poll();
+            int second = pq.poll();
+            pq.add(first + second * 2);
+        }
+        
+        if (pq.peek() < K) return -1;
+        
+        return answer;
+    }
+}
